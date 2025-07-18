305c4dde88f392c029891b7883e1f700fb93f1f2
Bruno <brunomartins1030@gmail.com>
2025-07-18 17:50:44 +0100

    adjustments on main.py and requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import cities
 from app.routers import photos
 from app.routers import users
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Inclusão das rotas
 app.include_router(cities.router)
 app.include_router(photos.router)
 app.include_router(users.router)
@@ -12,12 +14,21 @@ app.include_router(users.router)
 # Configurar o middleware CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Isso permite todos os domínios (não recomendado para produção)
+    allow_origins=["*"],  # Permite requisições de qualquer origem (cuidado em produção)
     allow_credentials=True,
-    allow_methods=["*"],  # Permitir todos os métodos (GET, POST, etc.)
-    allow_headers=["*"],  # Permitir todos os headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
+# Rota simples de teste
 @app.get("/")
 def read_root():
     return {"message": "Hello, world!"}
+
+# Bloco necessário para rodar com uvicorn, incluindo Railway
+if __name__ == "__main__":
+    import uvicorn
+    import os
+
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
